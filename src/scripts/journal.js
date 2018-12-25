bb2fd402a7f6295f42014d9f9fb38429120f6a2a
dek <deeeqhaji@gmail.com>
2018-12-24 20:44:13 -0600

    third journal but fetch have some issue

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,28 +1,5 @@
-const journalEntries = [
-    {
-        concept: "Array methods",
-        date: "07/24/2018",
-        entry: "We learned about array methods, but only forEach made sense",
-        mood: "Ok"
-    },
 
-    {
-        concept: "functions",
-        date: "10/24/2018",
-        entry: "We learned about functions, the logic is not clear for me",
-        mood: "excited"
-    },
-
-
-    {
-        concept: "protypes",
-        date: "11/24/2018",
-        entry: "We learned about protypes and how they function",
-        mood: "happy"
-    }
-]
-// console.log(journalEntries1)
-//////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 let entryRef = document.querySelector("entryLog");
 
 const makeJournalEntryComponent = (journalEntries) => {
